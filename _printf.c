b2500dbe9e90d759689782db418d536f76fd7d8a
Malik Badmus <46167222+Malikbadmus@users.noreply.github.com>
2022-11-20 12:51:10 +0100

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printf - print output
+ * @format: format string containing the characters and the specifiers
+ * Return: length of the formatted output string
+ */
+int _printf(const char *format, ...)
+{
+  return(0);
+}
