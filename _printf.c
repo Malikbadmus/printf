aa4156540b090883405e36684a700d9a797dcad3
Malik Badmus <malik.badmus@icloud.com>
2022-11-22 00:08:09 +0100

    project

--- _printf.c
+++ _printf.c
@@ -12,12 +12,30 @@
 int _printf(const char *format, ...)
 {
   int i = 0;
+  int count = 0;
+  int value = 0;
+
+  /*Prevent a Null pointer*/
+  if (format == NULL)
+        return (-1);
 
   while (format[i])
   {
-    write(1,&format[i],1);
-    i++;
-    
+
+      if (format[i] != '%')
+      {
+          count = write(1,&format[i],1);
+          count = count + value;
+          i++;
+          continue;
+      }
+
+      if (format[i] == '%')
+      {
+
+            printf("i have encountered a %\n");
+      }
+
   }
 
   return(0);
