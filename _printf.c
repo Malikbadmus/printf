55f191d9ae6c829492f347b4df437f4906fce9d1
Malik Badmus <46167222+Malikbadmus@users.noreply.github.com>
2022-11-21 06:43:15 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,11 +1,13 @@
 #include "main.h"
-
+#include <stdarg.h>
 /**
  * _printf - print output
  * @format: format string containing the characters and the specifiers
+ *@...: varidic parameters(unknown)
  * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
+  
   return(0);
 }
