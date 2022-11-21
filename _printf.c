65c722a4714426f369526a8f50fbaa53060bfab4
Malik Badmus <malik.badmus@icloud.com>
2022-11-21 07:17:37 +0100

    Team project

--- _printf.c
+++ _printf.c
@@ -1,13 +1,24 @@
 #include "main.h"
+#include <unistd.h>
 #include <stdarg.h>
+
 /**
  * _printf - print output
  * @format: format string containing the characters and the specifiers
  *@...: varidic parameters(unknown)
  * Return: length of the formatted output string
  */
+
 int _printf(const char *format, ...)
 {
-  
+  int i = 0;
+
+  while (format[i])
+  {
+    write(1,&format[i],1);
+    i++;
+    
+  }
+
   return(0);
 }
---
+++ test file/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main -test file
+*Program starts execution from above
+*
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+	_printf("string")
+}
