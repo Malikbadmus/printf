d175c40d54543205db5c8df895acec3097d37c53
Malik Badmus <46167222+Malikbadmus@users.noreply.github.com>
2022-11-22 23:57:30 +0100

    Update main.h

--- main.h
+++ main.h
@@ -3,6 +3,9 @@
 
 int _printf(const char *format, ...);
 
+
+char *convert(unsigned long int num, int base, int lowercase);
+
 int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
