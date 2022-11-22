b496c8f797f74099e5dddb58e21ae816ca38b533
emmylem <benjaminosondu55@gmail.com>
2022-11-22 21:52:25 +0000

    printf.h

---
+++ printf.h
@@ -0,0 +1,27 @@
+#ifndef PRINT_H
+#define PRINT_H
+#include <stdio.h>
+#include <stdarg.h>
+/**
+  * struct specify_print - specifies struct
+  * @correct: correct character
+  * @f: function
+  */
+
+typedef struct specify_print
+{
+	char *correct;
+	int (*f)(va_list);
+} spec;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int (*print_func(char b))(va_list vl);
+int print_c(va_list vl);
+int print_s(va_list vl);
+int print_percent(va_list vl);
+int print_d(va_list vl);
+int print_i(va_list vl);
+
+
+#endif
