688bd7fc63ab05f731830e9dd5c27d84c095418f
emmylem <benjaminosondu55@gmail.com>
2022-11-23 22:47:44 +0000

    print_rev.c

---
+++ print_rev.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "printf.h"
+#include "main.h"
+/**
+  * print_rev - prints in reverse
+  * @vl: argument
+  * Return: number of chars
+  */
+int print_rev(va_list vl)
+{
+	char *str;
+	int i, j = 0;
+
+	str = va_arg(vl, char*);
+	if (str == NULL)
+		str = ")llun(";
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		j++;
+	}
+	return (j);
+}
