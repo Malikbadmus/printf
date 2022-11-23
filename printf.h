4521d36ba61f1df845d43a6e2668cbb2bc0e2f49
emmylem <benjaminosondu55@gmail.com>
2022-11-23 23:19:51 +0000

    print_rev.c

--- printf.h
+++ printf.h
@@ -18,11 +18,19 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int (*print_func(char b))(va_list vl);
 int print_c(va_list vl);
-int print_s(va_list vl);
+int print_s(va_list s);
 int print_percent(va_list vl);
 int print_d(va_list vl);
 int print_i(va_list vl);
 int print_rev(va_list vl);
+int print_bin(va_list vl);
+int print_x(va_list vl);
+int print_rot13(va_list R);
+int print_X(va_list vl);
+int print_unsig(va_list vl);
+int print_octal(va_list vl);
+int print_dec(va_list vl);
+
 
 
 #endif
